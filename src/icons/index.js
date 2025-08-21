23655c26f8cabd3447d4feb9708c3292edf6979b
ekimcoskun <ekim.coskun19@gmail.com>
2025-08-22 00:34:34 +0300

    Add necessary assets and global styles

---
+++ src/icons/index.js
@@ -0,0 +1,4 @@
+export { default as PlusIcon } from "./plus.svg";
+export { default as EmployeeIcon } from "./employee.svg";
+export { default as DeleteIcon } from "./delete.svg";
+export { default as EditIcon } from "./edit.svg";
